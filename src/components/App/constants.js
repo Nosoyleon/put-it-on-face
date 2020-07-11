06c54e3217e9f219c9aa4134aec2076a176ed8a5
Leon Ceballos <leonceballos@Erre-MacBook-Pro-10.local>
2020-07-10 19:53:31 -0500

    Basic App structure

---
+++ src/components/App/constants.js
@@ -0,0 +1,76 @@
+import blueMonster from 'assets/images/masks/overlay-blue-monster.png';
+import clown from 'assets/images/masks/overlay-clown.png';
+import frankenstein from 'assets/images/masks/overlay-frankenstein.png';
+import pumpkin from 'assets/images/masks/overlay-pumpkin.png';
+import redMonster from 'assets/images/masks/overlay-red-monster.png';
+import skull from 'assets/images/masks/overlay-skull.png';
+import vampire from 'assets/images/masks/overlay-vampire.png';
+import werewolf from 'assets/images/masks/overlay-werewolf.png';
+
+export const DEFAULT_FACES = [
+  {
+    key: 'face1',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&h=750&q=60'
+  },
+  {
+    key: 'face2',
+    imageUrl:
+      'https://images.unsplash.com/photo-1495527400402-c7d3dc332654?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&h=750&q=60'
+  },
+  {
+    key: 'face3',
+    imageUrl:
+      'https://images.unsplash.com/photo-1551864490-a587c9f6f5e7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&h=750&q=60'
+  },
+  {
+    key: 'face4',
+    imageUrl:
+      'https://images.unsplash.com/photo-1546525848-3ce03ca516f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&h=300&q=60'
+  },
+  {
+    key: 'face5',
+    imageUrl:
+      'https://images.unsplash.com/photo-1544549269-414016cccf40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&h=300&q=60'
+  },
+  {
+    key: 'face6',
+    imageUrl:
+      'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&h=300&q=60'
+  }
+];
+
+export const DEFAULT_MAKS = [
+  {
+    key: 'blue-monster',
+    imageUrl: blueMonster
+  },
+  {
+    key: 'clown',
+    imageUrl: clown
+  },
+  {
+    key: 'frankenstein',
+    imageUrl: frankenstein
+  },
+  {
+    key: 'pumpkin',
+    imageUrl: pumpkin
+  },
+  {
+    key: 'red-monster',
+    imageUrl: redMonster
+  },
+  {
+    key: 'sull',
+    imageUrl: skull
+  },
+  {
+    key: 'vampire',
+    imageUrl: vampire
+  },
+  {
+    key: 'werewolf',
+    imageUrl: werewolf
+  }
+];
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from 'assets/images/logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import unselected from 'assets/images/default/user.png';
+import { DEFAULT_FACES, DEFAULT_MAKS } from './constants';
+import styles from './styles.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.mainWrapper}>
+      <h1 className="title is-1 has-text-centered mb-6">Póntelos!</h1>
+      <div className={styles.appContainer}>
+        <div className={styles.defaultImages}>
+          {DEFAULT_FACES.map(({ imageUrl, key }) => (
+            <img className={styles.faceImage} key={key} alt={key} src={imageUrl} />
+          ))}
+        </div>
+        <div className={styles.previewContainer}>
+          <img className={styles.previewImage} alt="face" src={unselected} />
+          <div className="file is-primary mt-5 is-large is-centered">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label className="file-label">
+              <input className="file-input" type="file" name="photo" />
+              <span className="file-cta">
+                <span className="file-icon">
+                  <i className="fas fa-upload" />
+                </span>
+                <span className="file-label">Sube una foto…</span>
+              </span>
+            </label>
+          </div>
+        </div>
+        <div className={styles.defaultImages}>
+          {DEFAULT_MAKS.map(({ imageUrl, key }) => (
+            <img className={styles.faceImage} key={key} alt={key} src={imageUrl} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
